13a40e3b4fde287343668eaf5f04db72c5275a76
Gaudio-Ney <gaudio.ney@gmail.com>
2020-03-30 15:46:36 -0300

    Inicialização do projeto Saude Terceira Idade.

---
+++ Saude-Terceira-Idade/Saude-Terceira-Idade/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Saude-Terceira-Idade
+//
+//  Created by Gáudio Ney on 30/03/20.
+//  Copyright © 2020 Gáudio Ney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+        print("Oi")
+    }
+
+}
+
