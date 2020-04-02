3f480645cfaa123a44269ef1fabf284fd497ec0b
Gáudio Uchôa Ney <gaudio.ney@gmail.com>
2020-04-02 18:43:23 -0300

    Criar telas, tab bar e barra de pesquisa.

---
+++ Saude-Terceira-Idade/README.md
@@ -0,0 +1,6 @@
+<<<<<<< HEAD
+Saude-Terceira-Idade
+=======
+# Saude-Terceira-Idade
+Apple Developer Academy 2020
+>>>>>>> master
---
+++ Saude-Terceira-Idade/Saude-Terceira-Idade/HomePage.swift
@@ -0,0 +1,18 @@
+//
+//  HomePage.swift
+//  Saude-Terceira-Idade
+//
+//  Created by Gáudio Ney on 02/04/20.
+//  Copyright © 2020 Gáudio Ney. All rights reserved.
+//
+
+import UIKit
+
+class homePageVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.gray
+        
+    }
+}
